9f9280c631b99bc5bdf2e92f929b9a1c9f549b77
GOWDRU <GOWDRU@GOWDRU-PC>
2017-03-14 12:31:50 +0530

    1231

--- GIT_DEMO/src/test/java/gowda99/GIT_DEMO/TestGit.java
+++ GIT_DEMO/src/test/java/gowda99/GIT_DEMO/TestGit.java
@@ -5,6 +5,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxProfile;
+import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.remote.CapabilityType;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.testng.annotations.Test;
@@ -19,16 +20,16 @@ public class TestGit {
 //		System.setProperty("webdriver.gecko.driver", ".//webdriver//geckodriver.exe");
 //		WebDriver driver = new FirefoxDriver(profile);
 		
-		DesiredCapabilities capabilities = DesiredCapabilities.chrome();
-		capabilities.setCapability(CapabilityType.ACCEPT_SSL_CERTS, true);
-		System.setProperty("webdriver.chrome.driver", "F:\\webdriver\\chromedriver.exe");
-		WebDriver driver = new ChromeDriver(capabilities);
-		System.out.println("Chrome Initialized :::::");
-		
-//		DesiredCapabilities capabilities = DesiredCapabilities.internetExplorer();
+//		DesiredCapabilities capabilities = DesiredCapabilities.chrome();
 //		capabilities.setCapability(CapabilityType.ACCEPT_SSL_CERTS, true);
-//		System.setProperty("webdriver.ie.driver", "F:\\webdriver\\IEDriverServer.exe");
-//		WebDriver driver = new InternetExplorerDriver(capabilities);
+//		System.setProperty("webdriver.chrome.driver", "F:\\webdriver\\chromedriver.exe");
+//		WebDriver driver = new ChromeDriver(capabilities);
+//		System.out.println("Chrome Initialized :::::");
+		
+		DesiredCapabilities capabilities = DesiredCapabilities.internetExplorer();
+		capabilities.setCapability(CapabilityType.ACCEPT_SSL_CERTS, true);
+		System.setProperty("webdriver.ie.driver", "F:\\webdriver\\IEDriverServer.exe");
+		WebDriver driver = new InternetExplorerDriver(capabilities);
 		
 		
 		driver.get("https://www.google.co.in/");
